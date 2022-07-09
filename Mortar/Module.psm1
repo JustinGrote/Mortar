e4b5e0875f2b09f7dc131917a8dba8d3d2c5d977
Justin Grote <JustinGrote@users.noreply.github.com>
2022-07-08 17:34:54 -0700

    Implement Package Provider in C# and move other functions to Utils (#4)

--- Mortar/Module.psm1
+++ Mortar/Module.psm1
@@ -13,7 +13,7 @@ Add-Type -Path $PSScriptRoot/../Output/*.dll
 Update-FormatData -PrependPath $PSScriptRoot/Types/*.format.ps1xml
 
 $publicFunctions = @()
-foreach ($ScriptPathItem in 'Classes', 'Private', 'Public') {
+foreach ($ScriptPathItem in 'Classes', 'Utils', 'Private', 'Public') {
     $ScriptSearchFilter = [io.path]::Combine($PSScriptRoot, $ScriptPathItem, '*.ps1')
     $ScriptExcludeFilter = { $PSItem -notlike '*.tests.ps1' -and $PSItem -notlike '*.build.ps1' }
     Get-ChildItem $ScriptSearchFilter |
--- Mortar/Private/Get-TemplateClient.ps1
+++ Mortar/Private/Get-TemplateClient.ps1
@@ -2,24 +2,34 @@ using namespace Microsoft.TemplateEngine.IDE
 using namespace Microsoft.TemplateEngine.Edge
 using namespace Microsoft.TemplateEngine.Abstractions.Installer
 using namespace System.Threading
+using namespace Mortar
 
 function Get-TemplateClient {
     [CmdletBinding()]
     [OutputType('Microsoft.TemplateEngine.IDE.Bootstrapper')]
     param(
-        #A virtual client doesnt persist its config to disk. Useful for CI or Testing.
-        [Switch]$Virtual
+        #A virtual client doesnt persist its config to disk.
+        [Switch]$Virtual = $true #TODO: Load all components but the default packages provider instead
     )
 
     if (-not $SCRIPT:TemplateClient) {
+        $SCRIPT:TemplateHost = [DefaultTemplateEngineHost]::new('Mortar', $version)
         $version = (Get-Module Mortar).Version
-        [Bootstrapper]$SCRIPT:TemplateClient = [Bootstrapper]::new(
-            [DefaultTemplateEngineHost]::new('Mortar', $version),
+        [Bootstrapper]$client = [Bootstrapper]::new(
+            $SCRIPT:TemplateHost,
             $Virtual, #virtualizeConfiguration
             $true, #loadDefaultComponents
             "$HOME/.config/powershell"
         )
+
+        $factory = [PowerShellModuleTemplatePackageProviderFactory]::new()
+        #Registers our Template Finder with the client.
+        $client.AddComponent(
+            [PowershellModuleTemplatePackageProviderFactory].GetInterface('ITemplatePackageProviderFactory'),
+            $factory
+        )
+        $SCRIPT:TemplateClient = $client
     }
+
     return $SCRIPT:TemplateClient
 }
-
